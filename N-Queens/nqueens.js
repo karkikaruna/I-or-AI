01ffd4c5fd16c7f58268d6d0cad9a951490e701f
karkikaruna <karunabot1@gmail.com>
2026-06-23 22:10:15 +0545

    injs

---
+++ N-Queens/nqueens.js
@@ -0,0 +1,67 @@
+function isSafe(board, row, col, n) {
+    let i, j;
+
+    // Check column
+    for (i = 0; i < row; i++) {
+        if (board[i][col] === 'Q') {
+            return false;
+        }
+    }
+
+    // Check top-left diagonal
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] === 'Q') {
+            return false;
+        }
+    }
+
+    // Check top-right diagonal
+    for (i = row, j = col; i >= 0 && j < n; i--, j++) {
+        if (board[i][j] === 'Q') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function nQueen(board, row, n) {
+    if (row >= n) {
+        return true;
+    }
+
+    for (let col = 0; col < n; col++) {
+        if (isSafe(board, row, col, n)) {
+            board[row][col] = 'Q';
+
+            if (nQueen(board, row + 1, n)) {
+                return true;
+            }
+
+            // Backtrack
+            board[row][col] = '.';
+        }
+    }
+
+    return false;
+}
+
+function printBoard(board, n) {
+    console.log("\nSolution:");
+    for (let i = 0; i < n; i++) {
+        console.log(board[i].join(" "));
+    }
+}
+
+const n = 8;
+
+
+const board = Array.from({ length: n }, () =>
+    Array(n).fill('.')
+);
+
+if (nQueen(board, 0, n)) {
+    printBoard(board, n);
+} else {
+    console.log("No Solution");
+}
