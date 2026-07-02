6d0e0e9981e82f83f31703edda56d8276a46d937
karkikaruna <karunabot1@gmail.com>
2026-07-02 22:42:44 +0545

    15 puzzle added

--- lab2/puzzle15.py
+++ lab2/puzzle15.py
@@ -1,76 +1,80 @@
 from heapq import heappush, heappop
-
 start=(
-    (5,8,2),
-    (1,0,3),
-    (4,7,6)
+    (12,1,2,15),
+    (11,6,5,8),
+    (7,10,9,4),
+    (0,13,14,3)
 )
 goal=(
-    (1,2,3),
-    (4,5,6),
-    (7,8,0),
+    (1,2,3,4),
+    (5,6,7,8),
+    (9,10,11,12),
+    (13,14,15,0)
 )
 
 def h1 (start):
     count=0
-    for i in range(3):
-        for j in range (3):
+    for i in range(4):
+        for j in range(4):
             if start[i][j]!=0 and start[i][j]!=goal[i][j]:
                 count=count+1
     return count
+
+
+
 def find_blank(start):
-    for i in range(3):
-        for j in range (3):
-            if start[i][j]==0 :
+    for i in range(4):
+        for j in range(4):
+            if start[i][j]==0:
                 return i,j
 
 def get_neighbours(start):
     x,y=find_blank(start)
     moves=[(-1,0), (0,-1), (1,0), (0,1)]
     neighbours=[]
-
-    for dx,dy in moves:
+    
+    for dx, dy in moves:
         nx=x+dx
         ny=y+dy
-        if 0<=nx<3 and 0<=ny<3:
+        if 0<=nx<4 and 0<=ny<4:
             board=[list(row) for row in start]
             board[x][y], board[nx][ny]=board[nx][ny], board[x][y]
             neighbours.append(tuple(map(tuple, board)))
-        
+
     return neighbours
-    
-    
+
 def display(start):
     for row in start:
         print(row)
     print()
 
 def aStar(start, heuristic):
-    pq = []
+    pq=[]
     heappush(pq, (heuristic(start), 0, start))
 
-    parent = {start: None}
-    g_cost = {start: 0}
+    parent={start: None}
+    g_cost={start: 0}
+    expanded=0
 
-    expanded = 0
 
     while pq:
-        f, g, current = heappop(pq)
-        expanded += 1
+        f,g,current=heappop(pq)
 
-        if current == goal:
+        if g > g_cost[current]:
+            continue
+        expanded+=1
+
+        if current==goal:
             return True, parent, expanded
-
         for neighbour in get_neighbours(current):
-            new_g = g + 1
+            new_g=g+1
 
             if neighbour not in g_cost or new_g < g_cost[neighbour]:
-                g_cost[neighbour] = new_g
-                f_cost = new_g + heuristic(neighbour)
-                heappush(pq, (f_cost, new_g, neighbour))
-                parent[neighbour] = current
-
-    return False, parent, expanded
+                g_cost[neighbour]=new_g
+                f_cost=new_g+ heuristic(neighbour)
+                heappush(pq, (f_cost, new_g,neighbour))
+                parent[neighbour]=current
+    return False , parent, expanded
 
 def reconstruct(parent, current):
     path=[]
@@ -79,14 +83,12 @@ def reconstruct(parent, current):
         current=parent[current]
     return path[::-1]
 
-goalFound,parent,expanded =aStar(start, h1)
+goalFound, parent, expanded=aStar(start, h2)
 
-if(goalFound):
+if (goalFound):
     print(f"Goal found after expanding {expanded} states.")
-    for state in reconstruct(parent, goal):
+    for state in reconstruct (parent, goal):
         display(state)
-
 else:
-    print(F"Goal state not found after expanding {expanded} states.")
+    print(f"Goal state not found after expanding {expanded } states.")
 
-   
